d90cdf807faf9442a9f8ed58182b4d7d4cb625ab
Raunak <raunakkumar6437@gmail.com>
2025-08-18 21:55:43 +0530

    added schema

--- zodschema/managedata.ts
+++ zodschema/managedata.ts
@@ -4,5 +4,7 @@ export const managedataschema = z.object({
     userid :z.string().min(1 , "userid must required").optional(),
     title : z.string().min(2 , "title must required "),
     codesnippet : z.string().min(2 , "snippet must required"),
-    solution: z.string().min(2 , "solution must required")
+    solution: z.string().min(2 , "solution must required"),
+    status: z.string().min(1 , "status must required"),
+    tag:z.string().min(1, "status must required")
 })
