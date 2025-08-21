aad25c437c61866e9df4cb25c1dd6073108cf2c1
Raunak <raunakkumar6437@gmail.com>
2025-08-22 03:31:46 +0530

    made : profile page and components

--- src/app/dassh/profile/page.tsx
+++ src/app/dassh/profile/page.tsx
@@ -1,63 +1,69 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import React from "react";
 
-const page = () => {
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { authClient } from "@/lib/auth-client";
+import { logout } from "../layout";
+
+const Page = () => {
+  const { data: session } = authClient.useSession();
   return (
     <div>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
         <Card className="rounded-2xl">
           <CardHeader>
-            <CardTitle>Account</CardTitle>
+            <CardTitle>Account Info </CardTitle>
           </CardHeader>
           <CardContent className="space-y-3">
-            <div className="flex items-center gap-3">
-              <div>
-                <div className="font-semibold">Raunak</div>
-                <div className="text-sm opacity-70">raunak@patchmind.dev</div>
-              </div>
+            <div className="flex items-center gap-2">
+              <Avatar>
+                <AvatarImage src={`${session?.user.image}`} />
+                <AvatarFallback>PM</AvatarFallback>
+              </Avatar>
             </div>
-            <div className="space-y-2">
-              <Label>Display Name</Label>
-              <Input defaultValue="Raunak" />
+            <div>
+              <Label className="text-lg">Name</Label>
+              <p className="text-sm text-neutral-500"> {session?.user.name}</p>
             </div>
-            <div className="space-y-2">
-              <Label>Workspace</Label>
-              <Input defaultValue="PatchMind" />
+            <div>
+              <Label className="text-lg">Email</Label>
+              <p className="text-sm text-neutral-500"> {session?.user.email}</p>
             </div>
-            <Button className="rounded-xl w-full">Save</Button>
+            <div>
+              <Label className="text-lg">Created at : </Label>
+              <p className="text-sm text-neutral-500">
+                {" "}
+                {session?.user.createdAt.toDateString()}
+              </p>
+            </div>
+
+            <Button onClick={logout} className="rounded-xl w-full">
+              Logout
+            </Button>
           </CardContent>
         </Card>
-        <Card className="md:col-span-2 rounded-2xl">
+
+        <Card className=" rounded-2xl">
           <CardHeader>
             <CardTitle>API & Preferences</CardTitle>
           </CardHeader>
-          <CardContent className="grid grid-cols-1 md:grid-cols-2 gap-4">
+          <CardContent className="grid grid-cols-1 md:grid-cols-1 gap-4">
             <div className="space-y-2">
-              <Label>OpenAI API Key</Label>
+              <Label>Enter your own API </Label>
               <Input type="password" placeholder="sk-..." />
-              <Button variant="secondary" className="rounded-xl">
-                Test Connection
-              </Button>
+              <Button className="rounded-xl w-full ">Save</Button>
             </div>
-            <div className="space-y-2">
-              <Label>Dark Mode</Label>
-              <div className="flex items-center gap-2">
-                
-              </div>
-            </div>
-            <div className="space-y-2 md:col-span-2">
-              <Label>Export Data</Label>
-              <div className="flex gap-2">
-                <Button className="rounded-xl" variant="secondary">
-                  JSON
-                </Button>
-                <Button className="rounded-xl" variant="secondary">
-                  CSV
-                </Button>
-              </div>
+            <div>
+              <CardContent className=" bg-blue-200/10 h-[8rem] rounded-xl p-4">
+                <h1 className="text-lg">Note:</h1>
+                <Label className="text-neutral-500 mt-2">
+                  To use the AI features you can use your own API..
+                </Label>
+              </CardContent>
             </div>
           </CardContent>
         </Card>
@@ -66,4 +72,4 @@ const page = () => {
   );
 };
 
-export default page;
+export default Page;
