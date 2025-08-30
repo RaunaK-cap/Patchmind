2467936a0d9aa33ce604ba808f44cc08958b3f33
Raunak <raunakkumar6437@gmail.com>
2025-08-31 01:01:26 +0530

    moved schema from pages

--- zodschema/managedata.ts
+++ zodschema/managedata.ts
@@ -13,3 +13,12 @@ export const managedataschema = z.object({
 export const LLM_prompt_message = z.object({
     message: z.string().min(1, "input must required").max(100, "word limits exceeds")
 })
+
+export const createErrorSchema = z.object({
+    title: z.string().min(2, "title must required"),
+  
+    codesnippet: z.string().min(2, "Codesnippet must required"),
+    solution: z.string().min(2, "Solution must required"),
+    tags: z.string().min(1, "Tag must required"),
+    Status: z.string().min(1, "Status must required"),
+  });
